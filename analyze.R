dc0f297636f2c065464f931d9b38aff5008bc932
Petr Kočí <petr.koci@gmail.com>
2014-01-26 23:14:50 +0100

    readme

--- analyze.R
+++ analyze.R
@@ -1,8 +1,12 @@
 require(XML)
 
-
 urls  <- read.csv("urls.csv", header=F)
 
-for (i in urls) {
-  html  <- htmlParse(i)
+for (i in urls$V1) {
+  xml  <- xmlParse(paste(i, "/rss", sep=""), encoding="cp1250")
+  print(i)
+  browser()
 }
+
+
+
